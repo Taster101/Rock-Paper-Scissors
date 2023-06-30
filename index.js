4d3f740861d8c351136c933ad3c71ea14b043b10
Taster101 <Davontahenderson1@gmail.com>
2023-06-30 08:14:05 -0500

    added game function andround function

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
 
 //Get computer choice
 function getComputerChoice (){
-
     let choice;
      choice = Math.floor(Math.random() * 3);
      if(choice == 1){
-         return 'Rock'
+         return 'rock'
      } else if (choice == 2){
-         return 'Paper'
+         return 'paper'
      } else {
-         return 'Scissors'
+         return 'scissors'
      }
 
 }
-// get user choice 
-function userChoice(choice){
-let hand;
-    hand = choice.toLowercase()
-
-
+// Play a round of game 
+function round(userChoice,computerChoice){ 
+userChoice = userinput()
+computerChoice = getComputerChoice()
+if(userChoice == 'rock' && computerChoice == 'scissors' || userChoice == 'paper' && computerChoice == 'rock' || userChoice == 'scissors' && computerChoice == 'paper'){
+       console.log(`you win ${userChoice} beats ${computerChoice}`)
+      return '++player'
+} else if (userChoice == computerChoice){
+    console.log('tie!')
+    return round()
+} else {
+    console.log( `you have lost ${computerChoice} beats ${userChoice}`) 
+    return 'player score'
 }
-
-
-
-
-console.log(getComputerChoice())
-
-//compare user and computer choice 
-
-
-//best out of 3 wins 
+}
+//Game function
+function game(){
+    let player = 0;
+    let computer = 0
+   for(let i = 0;i < 5;i++){
+       round()
+       
+   }
+}
+///Get user input
+function userinput(){
+    let input = prompt('Rock, Paper ,Scissors ?').toLowerCase()
+    console.log(input)
+    if(input == 'paper' || input == 'rock' || input == 'scissors'){
+        
+       return input
+    } else {
+        console.log('invalid input');
+        userinput();
+    }
+;
+}
+game()
+ 
