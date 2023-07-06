7eb60d7d5608a8876f69328383778f5d1ff6d813
Taster101 <Davontahenderson1@gmail.com>
2023-07-06 12:35:42 -0500

    UI bottons are connected to the game

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 //on botton click run game with user choice of button thas was click 
+//
 //track score until computer or player reaches 5 
 
 
@@ -9,8 +10,31 @@ let player = 0
 let computer = 0
 let decider = 0
 
+
 btn.forEach((buttons) => {
-buttons.addEventListener('click', game)
+buttons.addEventListener('click',  (e) => {
+    
+   
+        
+            round(e.target.name)
+            if(decider == 1 ){   
+                player++  
+            } else if ( decider == 2){
+                computer++
+            }
+            if(player == 5  ){
+                console.log('Player is the Winner!')
+                reset()
+                return "tes"
+            } else if(computer == 5){
+                console.log('Computer Wins the game!')
+                reset()
+                return "best"
+            }
+         }
+        
+)
+
 });
 
 //Get computer choice
@@ -29,17 +53,15 @@ function getComputerChoice (){
 
 }
 // Play a round of game 
-function round(userChoice,computerChoice){ 
+function round(userChoice){ 
     console.log(player)
-userChoice = userinput()
-computerChoice = getComputerChoice()
+
+let computerChoice = getComputerChoice()
 if(userChoice == 'rock' && computerChoice == 'scissors' || userChoice == 'paper' && computerChoice == 'rock' || userChoice == 'scissors' && computerChoice == 'paper'){
        console.log(`you win ${userChoice} beats ${computerChoice}`)
       decider = 1
 } else if (userChoice == computerChoice){
     console.log('tie!')
-    round()
-   
 } else {
     console.log( `you have lost ${computerChoice} beats ${userChoice}`) 
     decider = 2
@@ -47,20 +69,13 @@ if(userChoice == 'rock' && computerChoice == 'scissors' || userChoice == 'paper'
 }
 //Game function
 function game(){
-   
    for(let i = 0;i < 100;i++){
-       round()
-       if(decider == 1 ){
-           
-           player++
-           
+       round(userchoice)
+       if(decider == 1 ){   
+           player++  
        } else if ( decider == 2){
            computer++
-
        }
-    
-       
-
        if(player == 5  ){
            console.log('Player is the Winner!')
            reset()
@@ -74,18 +89,18 @@ function game(){
    }
 
 ///Get user input
-function userinput(){
-    let input = prompt('Rock, Paper ,Scissors ?').toLowerCase()
+// function userinput(input){
+//    console.log(input)
     
-    if(input == 'paper' || input == 'rock' || input == 'scissors'){
+//     if(input == 'paper' || input == 'rock' || input == 'scissors'){
         
-       return input
-    } else {
-        console.log('invalid input');
-        userinput();
-    }
-;
-}
+//        return input
+//     } else {
+//         console.log('invalid input');
+//         userinput();
+//     }
+// ;
+// }
 function reset() {
     computer = 0
     player = 0
